fa2a1cd3f0307f13510a48f024395f19878d859e
Felipe M <philmes@gmail.com>
2021-02-04 23:52:53 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
+import { Storage } from 'aws-amplify';
+
 function App() {
+  const [file, setFile] = useState({ fileUrl: '', file: '', filename: ''});
+
+  const handleChange = e => {
+    const currentFile = e.target.files[0];
+    setFile(
+      { 
+        fileUrl: URL.createObjectURL(currentFile),
+        file: currentFile,
+        filename: currentFile.name
+      }
+    );
+  }
+  
+  const saveFile = () => {
+    Storage.put(file.filename, file.file)
+    .then(() => {
+      console.log('successfully saved file!');
+      setFile({ fileUrl: '', file: '', filename: ''});
+    })
+    .catch(err => {
+      console.error('error uploading file!', err);
+    })
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        {/* <img src={downFile.fileUrl} alt="" /> */}
+        <h1 className="App-title">Welcome to React</h1>
+        <input type='file' onChange={handleChange} />
+        <React.Fragment>
+          <img src={file.fileUrl} alt="" />
+        </React.Fragment>        
+        <button onClick={saveFile}>Save File</button>
+      </header>      
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -4,6 +4,12 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import Amplify from 'aws-amplify';
+import awsconfig from './aws-exports';
+import '@aws-amplify/ui/dist/style.css';
+
+Amplify.configure(awsconfig);
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
